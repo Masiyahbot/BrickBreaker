b170a77dd51cb0411a4ccc4a7b4a32f954b1abe4
Grac3-k <122053510+Grac3-k@users.noreply.github.com>
2023-10-30 15:48:03 -0700

    10/30/23

--- ball.gd
+++ ball.gd
@@ -6,6 +6,8 @@ const VELOCITY_LIMIT = 40
 
 @export var ball_speed = 20
 @export var lifes = 3
+@export var death_zone: DeathZone
+@export var ui: UI
 
 var speed_up_factor = 1.05
 
@@ -15,6 +17,7 @@ var start_position: Vector2
 
 func _ready():
 	start_position = position
+	death_zone.life_lost.connect(on_life_lost)
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * ball_speed * delta)
@@ -29,3 +32,15 @@ func start_ball():
 	randomize()
 	
 	velocity = Vector2(randf_range(-1, 1), randf_range(-1, -1)).normalized() * ball_speed
+	
+func on_life_lost():
+	lifes -= 1
+	if lifes == 0:
+		pass
+		#GAME OVER
+	else:
+		reset_ball()
+
+func reset_ball(): 
+	position = start_position
+	velocity = Vector2.ZERO
