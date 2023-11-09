4f5cd5cf978e3cf3cca2056ffdf35b84c0a2f19b
Masiyahbot <madevoe@s.sfusd.edu>
2023-11-09 15:29:54 -0800

    PC #6

--- Scripts/level_definitions.gd
+++ Scripts/level_definitions.gd
@@ -3,10 +3,7 @@ extends Node
 var current_level = 1
 
 var level_1 = [
-	[5,5,5,5,5],
-	[1,2,2,3,4],
-	[0,1,2,3,0],
-	[0,4,0,2,0]
+	[0,1,0]
 ]
 
 var level_2 = [
@@ -17,3 +14,8 @@ var level_2 = [
 	[1,1,1,1,1,1,1,1]
 ]
 
+var levels = [level_1, level_2]
+
+func get_current_level():
+	print(levels[current_level - 1])
+	return levels[current_level - 1]
